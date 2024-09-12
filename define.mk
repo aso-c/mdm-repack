0a2caf57a17ceb57bf5937202212b65369ba47ae
Solomatov A.A <solomatov.andrew@nami.ru>
2024-09-12 15:08:29 +0400

    Import package directory-tree for 2.0.19+vera-1 build version of mdm debian package.

--- define.mk
+++ define.mk
@@ -2,16 +2,35 @@
 # Variable definition file
 # for repack mdm debian package
 #
+# deb-version - Debian package version number format
+# format:
+#   [epoch:]upstream-version[-debian-revision]
+#
+# Example:
+#   mdm_2.0.19+vera-1_amd64.deb
+#   ^^^ ^^^^^^ ^^^^ ^ ^^^^^ ^^^- pkg file extention
+#    |     |     |  |   +----- - architecture sign
+#    |     |     |  +--------- - debian version suffix
+#    |     |     +------------ - ubuntu distributive codename
+#    |     +------------------ - upstream version
+#    +------------------------ - package name
+#
 
+# package name
 PACKNAME = mdm
+# upstream version
 UPSTREAM = 2.0.19
+# ubuntu distributive codename
 DISTR  = vera
+# debian version suffix
 SUFFIX = 1
-
+# architecture sign
 ARCH   = amd64
 
-# archive fname extention
+# package file extention
 EXT = deb
 
-WorkingDir = deb-tree
+# Debian control information directory name
 DebCONTROL = DEBIAN
+# Working tree for expanded debian package
+WorkingDir = deb-tree
